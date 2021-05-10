d24ddb0cff2f7ee970fd44522086406e26853377
EmperorAkashi20 <rishabhsethiadsce@gmail.com>
2021-05-10 11:57:46 +0530

    Constants + theme added

---
+++ lib/Components/constants.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:passwordmanager/Components/size_config.dart';
+
+const kPrimaryColor = Color(0xFFF33633);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+const PrimaryCalculatorButtonColor = Color(0xFFF33633);
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+final headingStyle = TextStyle(
+  fontSize: getProportionateScreenWidth(28),
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  height: 1.5,
+);
+
+const defaultDuration = Duration(milliseconds: 250);
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kNoUserError1 = "Please Check your email or password";
+const String kSignUp = "If you don't have an account, please sign up";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kNotStrongPassError =
+    "Use at least 1 Upper Case, \n1 Lower Case, \n1 Number, \n1 special character";
+const String kMatchPassError = "Passwords don't match";
+const String kNamelNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+const String kAddressNullError = "Please Enter your address";
+const String kNoUserError = "No User Found";
+const String kPasswordNotValidError = "Please Check Your Password";
+const String kEmptyFieldError = "Please complete all details";
+const String kWrongPinError = "Please enter the correct PIN";
+
+final otpInputDecoration = InputDecoration(
+  contentPadding:
+      EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
+  border: outlineInputBorder(),
+  focusedBorder: outlineInputBorder(),
+  enabledBorder: outlineInputBorder(),
+);
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(getProportionateScreenWidth(15)),
+    borderSide: BorderSide(color: kTextColor),
+  );
+}
+
+class StringUrl {
+  static String testUrl =
+      "http://newsapi.org/v2/everything?domains=wsj.com&apiKey=API_KEY=52718bd9f5b94c5c9a0be48e44169e18";
+}
